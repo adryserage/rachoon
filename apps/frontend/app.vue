2d4c909a0f65de59df9e00d37d9674f703e702d0
Adis Durakovic <git@mail.adisdurakovic.com>
2025-10-08 21:29:54 +0200

    shortcuts

--- apps/frontend/app.vue
+++ apps/frontend/app.vue
@@ -4,9 +4,65 @@ useHead({
 });
 await useProfile().init();
 
-window.addEventListener("keyup", (e) => {
-  if (e.key === "Escape") {
+import { useMagicKeys } from "@vueuse/core";
+const navigate = ref("");
+
+const { N, G } = useMagicKeys();
+const { C, O, I, R, U, T, S, P } = useMagicKeys();
+const timer = ref<NodeJS.Timeout | null>(null);
+
+watch([N, G, C, O, I, R, U, T, S, P], ([N, G, C, O, I, R, U, T, S, P]) => {
+  if (!N && !G && !C && !O && !I && !R && !U && !T && !S && !P) return;
+  const activeElement = document.activeElement;
+  const isInput = activeElement instanceof HTMLInputElement || activeElement instanceof HTMLTextAreaElement;
+  let route = "";
+  if (isInput) return;
+
+  if (N || G) {
+    navigate.value = N ? "N" : "G";
+    if (timer.value) {
+      clearTimeout(timer.value);
+      timer.value = null;
+    }
+    timer.value = setTimeout(() => {
+      navigate.value = "";
+    }, 2000);
+
+    return;
   }
+  if (navigate.value === "") return;
+
+  if (C) {
+    route = "/clients";
+  }
+  if (O) {
+    route = "/offers";
+  }
+  if (I) {
+    route = "/invoices";
+  }
+  if (R) {
+    route = "/reminders";
+  }
+
+  if (U) {
+    route = "/users";
+  }
+  if (T) {
+    route = "/templates";
+  }
+  if (S) {
+    route = "/settings/organization";
+  }
+
+  if (P) {
+    route = "/profile";
+  }
+  if (navigate.value === "N" && !R && !S && !P) {
+    route += "/new";
+  }
+
+  useRouter().push(route);
 });
 </script>
 
---
+++ apps/frontend/components/KBCheatSheet.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul class="menu menu-xs">
+      <li>
+        <span>
+          <kbd class="kbd kbd-sm">N</kbd>
+          <span class="text-sm">Create New</span>
+        </span>
+        <ul>
+          <li>
+            <span>
+              <kbd class="kbd kbd-sm">C</kbd>
+              <span class="text-sm">Client</span>
+            </span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
