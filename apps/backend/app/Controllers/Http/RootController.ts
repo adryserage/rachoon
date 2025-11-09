c63e70ea75b5a0b59efc9c2adee37dc66188a8f6
adryserage <17680194+adryserage@users.noreply.github.com>
2025-11-09 14:57:04 -0500

    fix: return 200 with available flag instead of 404 for slug validation

--- apps/backend/app/Controllers/Http/RootController.ts
+++ apps/backend/app/Controllers/Http/RootController.ts
@@ -1,11 +1,11 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import OrganizationHelper from 'App/Helpers/organization'
 
-export default class AuthController {
+export default class RootController {
   public async index(ctx: HttpContextContract) {
     const organization = await OrganizationHelper.getFromContext(ctx)
     if (!organization) {
-      return ctx.response.notFound('No organization')
+      return ctx.response.ok({ available: true })
     } else {
       return organization
     }
